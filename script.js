6678bf37cf2298ad13bca289295fad24be598e5b
JO <jo@MBP-de-JO.lan>
2024-11-12 19:11:53 -0300

    creacion screen 8

--- script.js
+++ script.js
@@ -17,3 +17,44 @@ headers.forEach(header => {
         content.style.display = content.style.display === 'block' ? 'none' : 'block';
     });
 });
+
+let index = 0;
+const reseñas = document.querySelectorAll('.reseña');
+const totalReseñas = reseñas.length;
+
+function mostrarSiguienteReseña() {
+    index = (index + 1) % totalReseñas;
+    const carrusel = document.querySelector('.carrusel');
+    // Desplazar el carrusel en el eje X según la tarjeta actual
+    carrusel.style.transform = `translateX(-${(index * 100)}%)`;  // 100% porque las reseñas tienen margen
+}
+
+setInterval(mostrarSiguienteReseña, 3000);  // Cambiar cada 3 segundos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
